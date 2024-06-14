11e1b61b464589bb7ae334a96453b7fb4e4c69b6
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-06-13 21:45:19 -0700

    Update fixnano.sh

--- fixnano.sh
+++ fixnano.sh
@@ -1,4 +1,4 @@
--#!/bin/bash
+#!/bin/bash
 wget -c https://raw.githubusercontent.com/don-ferris/bash-scripts/main/etc-nanorc
-sudo mv ./etc-nanorc /etc/nanorc
-sudo nano /etc/nanorc
+mv ./etc-nanorc /etc/nanorc
+nano /etc/nanorc
