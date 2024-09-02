2ea7906ea9f94d31d09fc49ef44ea969a9f5389a
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-09-02 07:16:46 -0700

    Update fixnano.sh

--- fixnano.sh
+++ fixnano.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 wget -c https://raw.githubusercontent.com/don-ferris/bash-scripts/main/etc-nanorc
 if (( $EUID != 0 )); then
-    mv ./etc-nanorc /etc/nanorc
-    nano /etc/nanorc
-    exit
-else
     sudo mv ./etc-nanorc /etc/nanorc
     sudo nano /etc/nanorc
+else
+    mv ./etc-nanorc /etc/nanorc
+    nano /etc/nanorc
 fi
