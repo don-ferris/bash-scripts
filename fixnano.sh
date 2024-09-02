bd4385b906e006e5b17532b58db93aa996141085
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-09-02 07:09:52 -0700

    Update fixnano.sh - check if running as root
    If user is root, run commands without sudo

--- fixnano.sh
+++ fixnano.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 wget -c https://raw.githubusercontent.com/don-ferris/bash-scripts/main/etc-nanorc
-sudo mv ./etc-nanorc /etc/nanorc
-sudo nano /etc/nanorc
+if (( $EUID != 0 )); then
+    mv ./etc-nanorc /etc/nanorc
+    nano /etc/nanorc
+    exit
+else
+    sudo mv ./etc-nanorc /etc/nanorc
+    sudo nano /etc/nanorc
+fi
