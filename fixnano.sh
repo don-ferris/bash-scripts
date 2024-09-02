2d093bc9f86c11c1224e8d34c920592bcf0e1f3b
Don Ferris <109878120+don-ferris@users.noreply.github.com>
2024-09-02 07:02:48 -0700

    Update fixnano.sh to run commands with sudo

--- fixnano.sh
+++ fixnano.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -c https://raw.githubusercontent.com/don-ferris/bash-scripts/main/etc-nanorc
-mv ./etc-nanorc /etc/nanorc
-nano /etc/nanorc
+sudo mv ./etc-nanorc /etc/nanorc
+sudo nano /etc/nanorc
