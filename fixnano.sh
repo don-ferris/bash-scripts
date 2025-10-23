f7906a75c72235e5d614f16c285234130957b2f0
Donnie Ferris <109878120+don-ferris@users.noreply.github.com>
2025-10-23 12:26:32 -0500

    Enhance fixnano.sh with script description
    Add comments to explain the purpose of the script.

--- fixnano.sh
+++ fixnano.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# fixnano.sh
+# a script to download and install a modified /etc/nanorc config file to enable mouse support in the nano text editor and to enable common key bindings (e.g. Ctrl+X = cut; Ctrl+V = Paste; Ctrl+S = Save, Ctrl+F = Find, etc.)
 wget -c https://raw.githubusercontent.com/don-ferris/bash-scripts/main/etc-nanorc
 if (( $EUID != 0 )); then
     sudo mv ./etc-nanorc /etc/nanorc
